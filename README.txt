0f7ebc420abb94f31c2f8363c707c66fc52e7c2c
Brian Young <brianyoung@stanford.edu>
2013-02-14 16:29:42 -0800

    Added admin shortcut block region Added bootstrap dropdown classes to menu items

--- README.txt
+++ README.txt
@@ -1,3 +1,29 @@
-Stanford Seven Theme for Drupal
+Open Framework Subtheming Kit for Drupal 7.x
 
-master branch is unused. Please see versioned branches instead.
+-- SUMMARY -- 
+
+Authors: Megan Erin Miller
+This kit is for creating subthemes based on Open Framework (http://openframework.stanford.edu).
+
+-- REQUIREMENTS --
+
+This Stanford custom Drupal theme is intended for Drupal versions 7 only; it will not work with Drupal 6 or below. You must install Open Framework theme 7.x-2.x or higher for this theme to operate properly.
+
+-- SUBTHEMING INSTRUCTIONS --
+
+1) Copy (don't move) the subtheme_kit folder from the Open Framework theme folder.
+2) Paste it in your themes directory.
+3) Rename the "subtheme_kit" folder to "your-theme-name" and rename the "subtheme_kit" .info file to "your-theme-name" as well.
+4) Edit the README.txt for your new theme (this file) and the theme info in the .info file.
+5) Once you have renamed everything, you can proceed to the installation instructions below.
+
+To start creating your subtheme, you can use the CSS template included (.css file), or add your own. Add Javascript to the scripts folder. Add your own site logo to replace the logo.png file (keep same name) and your own theme's screenshot (replace screenshot.png). Also you can add a favicon.ico file for your site if you wish.
+
+-- INSTALLATION --
+
+As an admin, go to Administer > Appearance > Themes to enable your subtheme and the Open Framework theme (as it is a required base theme).
+More detailed information on installing themes here: http://drupal.org/getting-started/install-contrib/themes
+
+-- TROUBLESHOOTING --
+
+Connect with the Open Framework maintainers at http://openframework.stanford.edu.
---
+++ template.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Returns HTML for status and/or error messages, grouped by type.
+ */
+function stanford_seven_status_messages($variables) {
+  $display = $variables['display'];
+  $output = '';
+
+  $status_heading = array(
+    'status' => t('Status message'),
+    'error' => t('Error message'),
+    'warning' => t('Warning message'),
+  );
+
+  // Map Drupal message types to their corresponding Bootstrap classes.
+  // @see http://twitter.github.com/bootstrap/components.html#alerts
+  $status_class = array(
+    'status' => 'success',
+    'error' => 'error',
+    'warning' => 'info',
+  );
+
+  foreach (drupal_get_messages($display) as $type => $messages) {
+    $class = (isset($status_class[$type])) ? ' alert-' . $status_class[$type] : '';
+    $output .= "<div class=\"alert alert-block$class\">\n";
+    $output .= "  <a class=\"close\" data-dismiss=\"alert\" href=\"#\">x</a>\n";
+
+    if (!empty($status_heading[$type])) {
+      $output .= '<h2 class="element-invisible">' . $status_heading[$type] . "</h2>\n";
+    }
+
+    if (count($messages) > 1) {
+      $output .= " <ul>\n";
+      foreach ($messages as $message) {
+        $output .= '  <li>' . $message . "</li>\n";
+      }
+      $output .= " </ul>\n";
+    }
+    else {
+      $output .= $messages[0];
+    }
+
+    $output .= "</div>\n";
+  }
+  return $output;
+}
+
+function stanford_seven_menu_tree(&$vars) {
+  return '<ul class="menu nav">' . $vars['tree'] . '</ul>';
+}
+
+function stanford_seven_menu_link(array $vars) {
+  $element = $vars['element'];
+  $sub_menu = '';
+  
+  if ($element['#below']) {
+    // Ad our own wrapper
+    unset($element['#below']['#theme_wrappers']);
+    $sub_menu = '<ul class="dropdown-menu">' . drupal_render($element['#below']) . '</ul>';
+    $element['#localized_options']['attributes']['class'][] = 'dropdown-toggle';
+    $element['#localized_options']['attributes']['data-toggle'] = 'dropdown';
+
+    // Check if this element is nested within another
+    if ((!empty($element['#original_link']['depth'])) && ($element['#original_link']['depth'] > 1)) {
+      // Generate as dropdown submenu
+      $element['#attributes']['class'][] = 'dropdown-submenu';
+    }
+    else {
+      // Generate as standard dropdown
+      $element['#attributes']['class'][] = 'dropdown';
+      $element['#localized_options']['html'] = TRUE;
+      $element['#title'] .= ' <span class="caret"></span>';
+    }
+
+    // Set dropdown trigger element to # to prevent inadvertant page loading with submenu click
+    $element['#localized_options']['attributes']['data-target'] = '#';
+  }
+  
+  $output = l($element['#title'], $element['#href'], $element['#localized_options']);
+  return '<li' . drupal_attributes($element['#attributes']) . '>' . $output . $sub_menu . "</li>\n";
+}
