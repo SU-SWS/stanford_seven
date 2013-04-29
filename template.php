259d9aa1587d4c6153a1b3b26347bcb71f099394
Megan Miller <meganem@stanford.edu>
2013-04-29 10:54:36 -0700

    Removing references to Bootstrap

--- template.php
+++ template.php
@@ -2,34 +2,3 @@
 function stanford_seven_menu_tree(&$vars) {
   return '<ul class="menu nav">' . $vars['tree'] . '</ul>';
 }
-
-function stanford_seven_menu_link(array $vars) {
-  $element = $vars['element'];
-  $sub_menu = '';
-  
-  if ($element['#below']) {
-    // Ad our own wrapper
-    unset($element['#below']['#theme_wrappers']);
-    $sub_menu = '<ul class="dropdown-menu">' . drupal_render($element['#below']) . '</ul>';
-    $element['#localized_options']['attributes']['class'][] = 'dropdown-toggle';
-    $element['#localized_options']['attributes']['data-toggle'] = 'dropdown';
-
-    // Check if this element is nested within another
-    if ((!empty($element['#original_link']['depth'])) && ($element['#original_link']['depth'] > 1)) {
-      // Generate as dropdown submenu
-      $element['#attributes']['class'][] = 'dropdown-submenu';
-    }
-    else {
-      // Generate as standard dropdown
-      $element['#attributes']['class'][] = 'dropdown';
-      $element['#localized_options']['html'] = TRUE;
-      $element['#title'] .= ' <span class="caret"></span>';
-    }
-
-    // Set dropdown trigger element to # to prevent inadvertant page loading with submenu click
-    $element['#localized_options']['attributes']['data-target'] = '#';
-  }
-  
-  $output = l($element['#title'], $element['#href'], $element['#localized_options']);
-  return '<li' . drupal_attributes($element['#attributes']) . '>' . $output . $sub_menu . "</li>\n";
-}
